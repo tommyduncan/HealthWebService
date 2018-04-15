45085761723f00b26122f3895299328edefe21f9
Tommy <lamborghini801021@hotmail.com>
2018-04-16 00:30:24 +0800

    Add " login function\

--- models/AppUser.js
+++ models/AppUser.js
@@ -5,7 +5,7 @@ function define(sequelize, DataTypes) {
         id: { type: Sequelize.INTEGER, primaryKey: true, autoIncrement: true },
         Clinic_No: { type: Sequelize.INTEGER, primaryKey: true },
         App_Account: { type: DataTypes.STRING(10), allowNull: false },
-        App_Password: { type: DataTypes.STRING(10), allowNull: false }
+        App_Password: { type: DataTypes.STRING, allowNull: false }
     });
 
     return AppUser;
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const crypto = require('crypto');
 const { check, validationResult } = require('express-validator/check');
 const User = require('../models').AppUser;
 
-/* GET users listing. */
-router.get('/', function (req, res, next) {
-  res.send('respond with a resource');
-});
-
+/* User register */
 router.post('/register', [
   check('username').isString().not().isEmpty(),
   check('password').isString().not().isEmpty()
@@ -25,6 +22,31 @@ router.post('/register', [
       User.create({App_Account: req.body.username, App_Password: req.body.password});
 
       res.send('Register success.');
+    }
+  });
+});
+
+/* User login */
+router.post('/login', [
+  check('username').isString().not().isEmpty(),
+  check('password').isString().not().isEmpty()
+], (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).send('Invalid request body!');
+  }
+
+  User.findOne({ where: { App_Account: req.body.username } }).then(user => {
+    if (!user)
+      res.status(401).json({error: true, data: null, errorMsg: 'nonexistent user'});
+    else {
+      var hashedPassword = crypto.createHmac('sha256', 'HealthManagementSystem').update(req.body.password).digest('hex');
+
+      if(user.App_Password == hashedPassword)
+        res.send({error: false, data: 'login success', errorMsg: null});
+      else
+        res.status(401).json({error: true, data: null, errorMsg: 'error password'});
     }
   });
 });
