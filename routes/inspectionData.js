1bc0be6cc01c54c7de433c1d3b461e01f22f5d38
Tommy <lamborghini801021@hotmail.com>
2018-05-02 01:00:25 +0800

    InspectionData 新增撈取日期區間的檢測資訊 API

--- routes/inspectionData.js
+++ routes/inspectionData.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('express-jwt');
+const Op = require('../models').Sequelize.Op;
 const InspectionData = require('../models').InspectionData;
 
-router.get('/', jwt( {secret: 'HealthCare'} ), function (req, res, next) {
+router.get('/', jwt({ secret: 'HealthCare' }), function (req, res, next) {
     var payload = req.user;
 
-    InspectionData.findAll({where: {Clinic_No: payload.Clinic_No}}).then(inspectionDataList => {
+    InspectionData.findAll({ where: { Clinic_No: payload.Clinic_No } }).then(inspectionDataList => {
         res.json(inspectionDataList[inspectionDataList.length - 1]);
+    });
+});
+
+router.get('/:startDate/:endDate', jwt({ secret: 'HealthCare' }), function (req, res, next) {
+    var payload = req.user;
+
+    InspectionData.findAll({ where: { Clinic_No: payload.Clinic_No, InspectionDay: { [Op.gte]: req.params.startDate, [Op.lte]: req.params.endDate } } }).then(inspectionDataList => {
+        res.json(inspectionDataList);
     });
 });
 
